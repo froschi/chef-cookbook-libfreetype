4bc1a925e5681c931a34e9943fdda5d1e75f5212
Thorsten Fischer <thorsten@froschi.org>
2012-06-08 00:12:40 +0200

    Amended for use in lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,11 @@
-packages = %w/
-  libfreetype6
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid", "precise"
+  packages |= %w/
+    libfreetype6
+  /
+end
 
 packages.each do |pkg|
   package pkg do
